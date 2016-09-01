5374e8078aff531f27b24f75e59626e58fbd127f
rgarlock <ryan.garlock@milwaukeetool.com>
2016-09-01 10:13:33 -0500

    Started User Sevice.

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,22 @@
 import {bootstrap} from 'angular2/platform/browser';
-
+import {Http, Response} from 'angular2/http';
 import {Component} from 'angular2/core';
+import { UserService } from './users/users.service';
 
 @Component({
     selector: 'app',
-    template: `<div>Some Text</div>`
+    template: `<div>Some Text</div>`,
+    providers: [ UserService ]
 })
 
 export class AppComponent{
-
+    constructor(private _userService: UserService, private _http: Http) {
+        console.log(_http);
+        console.log(_userService);
+        this._userService = new UserService(_http);
+        var users = this._userService.getUsers('');
+        console.log(users);
+    }
 }
 
 bootstrap(AppComponent);
