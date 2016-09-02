2cbdffcb19b073b3c471e1351f52dec36b5d98df
rgarlock <ryan.garlock@milwaukeetool.com>
2016-09-01 19:40:14 -0500

    Added autocomplete to the list of users.

--- app/index.ts
+++ app/index.ts
@@ -9,15 +9,22 @@ import { UserComponent } from './users/user.component';
 
 @Component({
     selector: 'app-main',
+    styleUrls:  ['./content/index.component.css'],
     template:`
-    
 
     <div class="container">
         <div class="form-horizontal">
             <div class="form-group">
                 <label for="inputSearch" class="col-lg-2 col-md-2 col-sm-3 control-label">Search Criteria</label>                
                 <div class="col-lg-8 col-md-6 col-sm-9">
-                    <input class="form-control ng-untouched ng-pristine ng-valid" id="inputSearch" [(ngModel)]="searchTerm" placeholder="Email" type="text">
+                    <div>
+                        <input class="form-control ng-untouched ng-pristine ng-valid" id="inputSearch" (keyup)="keyup($event)" [(ngModel)]="searchTerm" placeholder="Email" type="text">
+                        <div class="autocomplete" *ngIf="autoCompleteList">
+                            <div *ngFor="let user of autoCompleteList; let index=index; let odd=odd; let even=even;" 
+                                    [ngClass]="{ 'even': even, selected: index == selectedAutoCompleteIndex }" class="autocomplete-item" 
+                                    (click)="autocompleteClick(user.email)">{{ user.email }}</div>
+                        </div>
+                    </div>
                 </div>
                 <button class="col-lg-2 col-md-2 col-sm-3 btn btn-primary" (click)="search()" type="submit">Submit</button>
             </div>
@@ -32,6 +39,7 @@ import { UserComponent } from './users/user.component';
 export class IndexComponent{
     searchTerm: string;
     user: IUser;
+    autoCompleteList: IUser[];
     private _userService : UserService;
 
     constructor(@Inject(forwardRef(() => UserService)) _userService,
@@ -40,11 +48,55 @@ export class IndexComponent{
 
     }
 
+    autocompleteClick(email: string): void {
+        this.searchTerm = email;
+        this.search();
+    }
+
     search(): void {
         this.user = undefined;
+        this.autoCompleteList = undefined;
         this._userService.getUser(this.searchTerm).subscribe((result) => {
             if (result) {
                 this.user = result;
+            }
+        }, (err) => {
+            console.log(err);
+        });
+    }
+
+    selectedAutoCompleteIndex: number = -1;
+    keyup($event): void {
+        var keyCode = $event.keyCode;
+        switch (keyCode) {
+            case 13:
+                if (this.selectedAutoCompleteIndex > -1) {
+                    this.searchTerm = this.autoCompleteList[this.selectedAutoCompleteIndex].email;
+                }
+                this.search();
+                break;
+            case 40:
+                var max = this.selectedAutoCompleteIndex + 1;
+                if (this.autoCompleteList && max != this.autoCompleteList.length) {                    
+                    this.selectedAutoCompleteIndex++;
+                }
+                break;
+            case 38:
+                if (this.autoCompleteList && this.selectedAutoCompleteIndex > -1) {
+                    this.selectedAutoCompleteIndex--;
+                }
+                break;
+            default:
+                this.autoComplete();
+                break;
+        }
+    }
+
+    autoComplete(): void {
+        this.selectedAutoCompleteIndex = -1;
+        this._userService.getAutoComplete(this.searchTerm).subscribe((result) => {
+            if (result) {
+                this.autoCompleteList = result;
             }
         }, (err) => {
             console.log(err);
